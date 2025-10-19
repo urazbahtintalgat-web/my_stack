491b5d4e8079e78166cb2d71550ce7b492f6d555
T0g1r <T0g1r@yandex.ru>
2025-10-19 16:40:50 +0300

    разбил асемблер на функции чтобы сделать почитабильнее и покороче

--- asembler.cpp
+++ asembler.cpp
@@ -26,8 +26,151 @@ int fwrite_register(const char * now_register, FILE * machine_text) {
             return 1;
         }
     }
+    printf("ERROR: register was not found, %s:%d\n", __FILE__, __LINE__);
     return 0;
 }
+
+/**
+ * @brief Функция которая проверяет метка ли идет на этой строке
+ * 
+ * @return Возврацает | 1 если это метка | 0 если это не метка | -1 при ошибке
+ */
+int check_label(struct line * comands, int labels[], size_t i, int ip) {
+    if (comands[i].begin[0] == ':') {
+        int label = atoi(comands[i].begin + 1);
+        if (label >= 0 && label < LABEL_AMOUNT) {
+            labels[label] = ip;
+            printf("line %2d label :%d -> ip%d\n", (int)i, label, labels[label]);
+        } else {
+            printf("\nlabel %d out of range (0-%d)\n", label, LABEL_AMOUNT - 1);
+            return -1;
+        }
+        return 1;
+    }
+    return 0;
+}
+
+int write_comand(FILE * machine_text, int cmd, int * ip, int * comand_found, int double_run) {
+    printf("(number %2d) comand %9s ", *ip, ComandNames[cmd]);
+    *comand_found = 1;
+    if (double_run == 1 && fwrite(&cmd, sizeof(int), 1, machine_text) != 1) {
+        printf("assembler fwrite error %s:%d\n", __FILE__, __LINE__);
+        return 1;
+    }
+    *ip += 1;
+    return 0;
+}
+
+int check_jumps(FILE * machine_text, struct line * comands, int labels[], size_t i, int cmd, int now_comand_length, int * ip, int double_run) {
+    if (ASM_JMP <= cmd && cmd <= ASM_CALL) {
+        const char * label_start = comands[i].begin + now_comand_length;
+
+        while (*label_start == ' ' || *label_start == '\t')
+            label_start++;
+
+        if (double_run == 1) {
+            if (*label_start == ':') {
+                int label = atoi(label_start + 1);
+                if (fwrite(&labels[label], sizeof(int), 1, machine_text) != 1) {
+                    printf("ERROR: jump ip fwrite error %s:%d\n", __FILE__, __LINE__);
+                    return 1;
+                }
+                printf("(number %2d) jump -> :%d(ip%d)", *ip, label, labels[label]);
+            } else {
+                int jump_ip = atoi(label_start);
+                if (fwrite(&jump_ip, sizeof(int), 1, machine_text) != 1) {
+                    printf("ERROR: jump ip fwrite error %s:%d\n", __FILE__, __LINE__);
+                    return 1;
+                }
+                printf("(number %2d)jump -> ip%d", *ip, jump_ip);
+            }
+        }
+        *ip += 1;
+    }
+    return 0;
+}
+
+int check_arguments(FILE * machine_text, struct line * comands, int labels[], size_t i, int cmd, int now_comand_length, int * ip, int double_run) {
+    if (cmd == ASM_PUSH) {
+        int value = atoi(comands[i].begin + now_comand_length);
+        printf("(number %2d) %d", *ip , value);
+        if (double_run == 1 && fwrite(&value, sizeof(int), 1, machine_text) != 1) {
+            printf("assembler fwrite error with push value %s:%d\n", __FILE__, __LINE__);
+            return 1;
+        }
+        *ip += 1;
+        return 0;
+    }
+    if (cmd == ASM_PUSHR || cmd == ASM_POPR) {
+        printf("(number %2d) ", *ip);
+        if (double_run == 1 && !fwrite_register(comands[i].begin + now_comand_length + 1, machine_text)) {
+            printf("assembler fwrite error with pushr value %s:%d\n", __FILE__, __LINE__);
+            return 1;
+        }
+        *ip += 1;
+        return 0;
+    }
+
+    if (cmd == ASM_PUSHM || cmd == ASM_POPM) {
+        printf("(number %2d) ", *ip);
+        const char * index_start = comands[i].begin + now_comand_length;
+
+        while (*index_start == ' ' || *index_start == '\n')
+            index_start++;
+        if (*index_start++ != '[') {
+            printf("ERROR: comand PUSHM/POPM continue without [\n");
+            printf("ERROR: %s:%d\n", __FILE__, __LINE__);
+            return 1;
+        }
+
+        int was_register = 0;
+        if (isalpha(*index_start))
+            was_register = 1;
+        
+        printf("was register = %d ", was_register);
+        if (double_run == 1 && fwrite(&was_register, sizeof(int), 1, machine_text) != 1) {
+            printf("ERROR: assembler fwrite error with registr flag %s:%d\n", __FILE__, __LINE__);
+            return 1;
+        }
+        *ip += 1;
+
+        switch (was_register) {
+            case 1: {
+                printf("(number %2d) ", *ip);
+                if (double_run == 1 && !fwrite_register(index_start, machine_text)) {
+                    printf("ERROR: assembler fwrite error with register value %s:%d\n", __FILE__, __LINE__);
+                    return 1;
+                }
+                *ip += 1;
+                return 0;
+                break;
+            }
+
+            case 0: {
+                printf("(number %2d) ", *ip);
+                int value = atoi(index_start);
+                printf("%d", value);
+                if (double_run == 1 && fwrite(&value, sizeof(int), 1, machine_text) != 1) {
+                    printf("assembler fwrite error with pushr value %s:%d\n", __FILE__, __LINE__);
+                    return 1;
+                }
+                *ip += 1;
+                return 0;
+                break;
+            }
+
+            default:
+                printf("ERROR: unknown error with pushm/popm %s:%d\n", __FILE__, __LINE__);
+                return 1;
+                break;
+        }
+    }
+
+    return 0;
+}
+
+
+
 
 int main() {
     int labels[LABEL_AMOUNT] = {0};
@@ -68,6 +211,14 @@ int main() {
 
 
         for (size_t i = 0; i < comand_amount; i++) {
+            //НАЧАЛО ПРОВЕРКИ НА МЕТКУ
+            int label_res = 0;
+            if ((label_res = check_label(comands, labels, i, ip))) {
+                if (label_res == -1) printf("Soft ERROR label was not detected don't miss that\n\n");
+                continue;
+            }
+            //КОНЕЦ ПРОВЕРКИ НА МЕТКУ
+            /*
             if (comands[i].begin[0] == ':') {//ПРОПИСАТЬ ЕСЛИ ЭТО МЕТКА
                 int label = atoi(comands[i].begin + 1);
                 if (label >= 0 && label < LABEL_AMOUNT) {
@@ -78,7 +229,7 @@ int main() {
                     return 1;
                 }
                 continue;
-            }
+            }*/
 
             int now_comand_length = 0;
             while (now_comand_length < COMANDS_MAX_LENGTH && 
@@ -88,21 +239,22 @@ int main() {
                    comands[i].begin[now_comand_length] != '\n') {
                 now_comand_length++;
             }
-            if (now_comand_length == 0) {//если например пропуск строки
-                continue;
-            }
+            if (now_comand_length == 0) continue;//ПРОПУСК СТРОКИ
             printf("line %2d ", (int)i);////////////////////
 
-            int command_found = 0;
+            int command_found = 0;//ФЛАГ ДЛЯ ПРОВЕРКИ НАШЛАСЬ ЛИ КОМАНДА
 
             for (int cmd = 0; cmd < (sizeof(ComandNames) / sizeof(ComandNames[0])); cmd++) {
-                if (ComandNames[cmd][0] == '\0')
-                    continue;
-                if (strncmp(comands[i].begin, ComandNames[cmd] + 4, now_comand_length) != 0 || now_comand_length != strlen(ComandNames[cmd]) - 4)
-                    continue;
+                //ПРОПУСТИТЬ ЕСЛИ ЭТО ПУСТАЯ КОМАНДА
+                if (ComandNames[cmd][0] == '\0') continue;
+                //ПРОПУСТИТЬ ЕСЛИ КОМАНДА НЕ СОВПАЛА
+                if (strncmp(comands[i].begin, ComandNames[cmd] + 4, now_comand_length) != 0 || now_comand_length != strlen(ComandNames[cmd]) - 4) continue;
 
                 
-                //ЗАПИСЬ КОМАНТЫ В ТЕКСТОВИК ДЛЯ ПРОЦЕССОРА
+                //ЗАПИСЬ КОМАНДЫ В ТЕКСТОВИК ДЛЯ ПРОЦЕССОРА
+                if (write_comand(machine_text, cmd, &ip, &command_found, dobule_run)) return 1;
+                //КОНЕЦ ЗАПИСИ ДАЛЕЕ ИДУТ ПРОВЕРКИ НА АРГУМЕНТЫ
+                /*
                 printf("(number %2d) comand %9s ", ip, ComandNames[cmd]);//////////////
                 command_found = 1;
                 if (dobule_run == 1 && fwrite(&cmd, sizeof(int), 1, machine_text) != 1) {
@@ -110,15 +262,19 @@ int main() {
                     return 1;
                 }
                 ip++;
-                //КОНЕЦ ЗАПИСИ ДАЛЕЕ ИДУТ ПРОВЕРКИ НА АРГУМЕНТЫ
+                */
 
 
                 //----------------------
                 //ПРОВЕРКИ НА АРГУМЕНТЫ:
                 //----------------------
-                    
-                    
+                
+                
                 //ПРОВЕРКИ НА ПРЫЖКИ И ЗАПИСИ АЙПИШНИКОВ ПРИЖКОВ
+                if (check_jumps(machine_text, comands, labels, i, cmd, now_comand_length, &ip, dobule_run)) return 1;
+                
+                //КОНЕЦ ПРОВЕРКИ НА ПРЫЖКИ
+                /*
                 if (ASM_JMP <= cmd && cmd <= ASM_CALL) {
                     const char * label_start = comands[i].begin + now_comand_length;
 
@@ -129,7 +285,7 @@ int main() {
                         if (*label_start == ':') {
                             int label = atoi(label_start + 1);
                             fwrite(&labels[label], sizeof(int), 1, machine_text);
-                            printf("(number %2d) jump -> :%d", ip, labels[label]);
+                            printf("(number %2d) jump -> :%d(ip%d)", ip, label, labels[label]);
                         } else {
                             int jump_ip = atoi(label_start);
                             fwrite(&jump_ip, sizeof(int), 1, machine_text);
@@ -138,11 +294,14 @@ int main() {
                     }
                     ip++;
                 }
-                //КОНЕЦ ПРОВЕРКИ НА ПРЫЖКИ
+                    */
 
 
                 //НАЧАЛО ПРОВЕРКИ НА РАБОТУ С АРГУМЕНТАМИ ВНУТРИ ПРОЦЕССОРА
-
+                if (check_arguments(machine_text, comands, labels, i, cmd, now_comand_length, &ip, dobule_run)) return 1;
+                
+                //КОНЕЦ  ПРОВЕРКИ НА РАБОТУ С ОПЕРАТИСНОЙ ПОМЯТЬЮ
+                /*
                 switch (cmd) {
                     case ASM_PUSH: {
                         int value = atoi(comands[i].begin + now_comand_length);
@@ -303,7 +462,8 @@ int main() {
                 default:
                     break;
                 }
-                //КОНЕЦ  ПРОВЕРКИ НА РАБОТУ С ОПЕРАТИСНОЙ ПОМЯТЬЮ
+                */
+                
 
 
                 printf("\n");////////////////
